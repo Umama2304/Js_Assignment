f7e77aa53362de75f39c8ed3d5519267fefd9406
Umama2304 <129634648+Umama2304@users.noreply.github.com>
2023-05-06 12:09:36 +0500

    Add files via upload

---
+++ Javascript Assignment 1/assets/app.js
@@ -0,0 +1,15 @@
+// Question 1;
+// alert("Hello! Welcome to our site!")
+// Question 2
+// alert("Error!Please enter a valid password")
+// Question 3
+// alert("Welcome to Js Land..\n Happy Coding!")
+// Question 4 
+// alert("Welcome to Js Land...")
+// alert("Happy Coding \n prevent this page from from creating additional dialogs")
+// Question 5
+// console.log("Hello... I can run JS through my web browser's console");
+// Question 6
+// alert("Happy Coding!")
+// Question 7
+// Answer on index.html File
