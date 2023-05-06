5127f889cd940aae961c7910fdce8c8be0016133
Umama2304 <129634648+Umama2304@users.noreply.github.com>
2023-05-06 12:10:31 +0500

    Add files via upload

---
+++ Javascript Assignment 2/assets/app.js
@@ -0,0 +1,27 @@
+// Question 1
+// var Username
+// Question 2
+// var myName= "Umama Noor"
+// Question 3 
+// var message
+// message= alert ("Hello World!")
+// Question 4
+// var studentName="Jhone Doe"
+// alert(studentName)
+// var studentAge="15 years old"
+// alert(studentAge)
+// var studentQualification="Certified Mobile Application Development"
+// alert(studentQualification)
+// Question 5
+
+// Question 6
+// var email="umamanoor246@gmail.com"
+// alert("My email address is"+ email)
+// Question 7
+// var book="A smarter way to learn Javascript"
+// alert("I am trying to learn from the Book" + book)
+// Question 8
+// document.write("Yah! I can write HTML content through JS")
+// Question 9
+// var string=“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”
+// alert(string)
