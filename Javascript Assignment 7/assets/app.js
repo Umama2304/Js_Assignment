2548be741116707bd31c9b458498f1be268ba9f1
Umama2304 <129634648+Umama2304@users.noreply.github.com>
2023-05-17 14:22:46 +0500

    Add files via upload

---
+++ Javascript Assignment 7/assets/app.js
@@ -0,0 +1,144 @@
+//  Chap 9/11 ;
+
+////Task # 1;  
+        // var cityName = prompt ( "Enter City ");
+        // if ( cityName== "karachi") {
+        //     alert( " City of Lights!")
+        // }
+
+//  ////Task # 2;
+        //  var gender = prompt ( "Type your Gender");
+        //  if (gender == "male") {
+        //     alert( " Morning Sir! ");
+        //  } else if (gender == "female") { 
+        //     alert ( " Morning Ma'am! ")
+        //  }
+
+
+////Task # 3; 
+        // var signalColor = prompt ( "Enter Traffic Signal color");
+        // if (signalColor == "red") {
+        //     alert ( "Must Stop")
+        // } 
+        //  else if (signalColor == "yellow") {
+        //     alert ( " Ready to move")
+        // }
+        // else if (signalColor == "green") {
+        //     alert ( " Move now")
+        // }
+
+ //// Task #4 ; 
+        //  var carFuel = +prompt (" Enter Fuel in litre" );
+        //  if (carFuel < 0.25  ) {
+        //     alert ( " You need more fuel"); }
+
+ //// Task # 5 ;
+        //a) condition is true;
+        //b)  condition is false;
+        //c)  Condition is 2 and 4 are true;
+        //d)  condition is true;
+        //e)  condition is true;
+        //f)  condition is true;
+
+
+ //// Task # 6; 
+            // document.write ( "<h1> MarkSheet</h1> <br>");
+            //     var sub1Mark = +prompt ( " Enter 1st Subject Mark");  sub2Mark = +prompt (" Enter 2nd Subject Mark" ); sub3Mark = +prompt (" Enter 3rd Subject Mark" );
+            //     var totalMarks = +prompt (" Enter Total Marks" );
+            //     var obtainMarks = sub1Mark + sub2Mark + sub3Mark ;  
+            //     var percent =  (obtainMarks * 100) / totalMarks;
+            //     var grade8 = " A-one" ; grade7 = " A" ; grade6 = " Fail";
+            //     var remarkAOne = " Exellent" ; remarkA = "Good " ; remarkB = "Sorry " ; 
+                
+            //     if ( percent >= 80) { 
+            //         document.write("<br>" + grade8 + " <br>" +  remarkAOne + "<br>" )   }
+            //     else if (percent >= 70) {
+            //         document.write("<br>"+ grade7 + " <br>"  +  remarkA + "<br>")  }
+                
+            // if (percent <= 60) {  
+            //         document.write("<h2>Total marks : " + totalMarks + "</h2>\n");
+            //         document.write("Marks obtained : " + obtainMarks + "\n");
+            //         document.write("Percentage : " + percent+ "% \n");
+            //         document.write("<br> Grade : Fail\n");
+            //         document.write("<h2>Remarks : Sorry</h2>\n");
+            //     }
+                
+            //    document.write ( " <br> <h3>Total Marks " + totalMarks + "</h3> ");
+            //     document.write( "<br> <h3> Marks Obatined: " + obtainMarks  + "</h3> ");
+
+            //     document.write( "<br><b> Percentage " + percent + " % </b>");
+
+
+// ////Task # 7; Guess Game
+//      var guess = +prompt(" Guess a number from 1 to 10" ); 
+        //      var secretNum = 9 ; 
+        //      if (guess == secretNum) {
+        //         alert ( " Bingo!")
+        //      }
+        //      else if (guess == secretNum +1  ) {
+        //         alert ( " CLose enough to correct answer.. ")
+        //      }
+    
+
+//// Task # 8 ; 
+    //  var num = +prompt (" Enter a Number" );
+    //  if (num % 3 === 0) {
+    //     alert ( " Divisible by 3")
+    //  } 
+    // else{
+    //     alert( num + " not divisible by 3")
+    // }
+
+
+//  /////Task # 9 ;
+    //  var evenNum = " Even Number";  oddNum = " Odd Number" ;
+    //  var userNum = +prompt ( " ENTER NUMBER" );
+    //  if (userNum % 2 ===0 ) {
+    //     alert ( evenNum)
+    //  }
+    //  else  {
+    //     alert ( oddNum)
+    //  }
+
+
+//// Task # 10; 
+    //   var temp = +prompt (" ENTER NUMBER" );
+    //   if (temp > 40) {
+    //     document.write ( " It is too Hot outside")
+    //   } else if (temp > 30) { 
+    //     document.write( "The Weather today is NorMal")
+    //   }
+    //   else if (temp > 20) {
+    //     document.write ( "Today's weather is Cool")
+    //   }
+    //     else if (temp > 10) {
+    //     document.write ( "The temperature is below 10 degrees")
+    //   }
+
+
+
+  //// Task # 11; 
+    //   var firstNum = +prompt( " ENTER First NUMBER "); secondNum = +prompt(" ENTER Second NUMBER " );
+    // operation = +prompt (  " ENTER the operation (+, -, *, /, %): ")
+    //  var result;
+    //             if (operation === "+") {
+    //             result = firstNum + secondNum;
+    //             }
+    //                  else if (operation === "-") {
+    //                  result = firstNum - secondNum;
+    //                  }
+    //                  else if (operation === "*") {
+    //                  result = firstNum * secondNum;
+    //                 }
+    //                 else if (operation === "/") {
+    //                 result = firstNum / secondNum;
+    //                 } 
+    //                 else if (operation === "%") {
+    //                 result = firstNum % secondNum; }
+    //     else {
+    //                 alert ("Invalid operation");
+    //                 }
+    //                  if (result !== undefined) {
+    //                 alert("The result is: " + result);
+    //                 }
+    
