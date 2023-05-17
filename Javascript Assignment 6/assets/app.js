3c622c44a65bf73d66c9bf71be206cd32facd2d6
Umama2304 <129634648+Umama2304@users.noreply.github.com>
2023-05-17 14:18:15 +0500

    Add files via upload

---
+++ Javascript Assignment 6/assets/app.js
@@ -0,0 +1,66 @@
+// Chap 6-9 Math Expressions ;
+
+ //Task: 1;            
+            //    var a = 10;
+            //     document.write("Result:<br> The value of a is:  " + a );
+            //     document.write("<br>............................................<br>");
+
+            //     var a = ++a ;
+            //     document.write("The value of ++a is: " + a);
+            //     document.write("<br> Now the value of a is " + a );              
+            //     document.write("<br>________________________________<br>");
+
+            //     var a_2 = a++;
+            //     document.write("<br> The value of a++ is: " + a_2 );
+            //     document.write("<br> Now the value of a is:  " + a); 
+            //     document.write("<br>________________________________<br>");
+
+            //     var a_3 = --a;
+            //     document.write("<br>The value of --a is: " + a);
+            //     document.write("<br> Now the value of a is " + a );
+            //     document.write("<br>________________________________<br>");
+                
+            //     var a_4 = a--;
+            //     document.write("<br>The value of a-- is: " + a_4);
+            //     document.write(" <br> Now the value of a is " + a);
+            //     document.write("<br>________________________________<br>");
+
+
+// Task : 2;    
+    // var a = 2, b = 1;  var result = --a - --b + ++b + b--;
+    // document.write( "a is " + a + "<br>" + " b is " + b + "<br>" + " result is " + result);        
+    //         //It subtracts 1 from (a). So, a becomes 1.
+            //Subtract 1 from (a) and 1 from (b) resulting in 0. So it becomes 0.         
+            // 0 + 1, equals 1. (++b) adds 1 to b resulting in 1. 
+            //The value of b (which is 1) in the above step, and then it is decremented by 1.
+            // It becomes 0 - 0 + 1 + 1, which equals 2.
+                    
+        
+ //Task :3 
+        //  var userName= prompt("Enter your  Name" );
+        //  var userName= alert(" Welcome! Back " + userName);
+            
+
+ //Task :5;
+            // var table= +prompt( 'Which multiplication table you want?', "5")
+            //   for ( var i = 1; i< 11 ; i++){
+            //     document.write( table + "x" + i + "=" + table*i  + "<br>") } 
+
+ // Task:6 ; 
+    //  var subject1 = prompt ( " Enter a subject Name 1" );var subject2 = prompt ( " Enter a subject Name 2" ); var subject3 = prompt ( " Enter a subject Name 3" );
+    //      var totalMarks = 100;
+    //     var obtainMarksSubject1 = prompt("Enter obtained marks for " + subject1 + ":");
+    //     var obtainMarksSubject2 = prompt("Enter obtained marks for " + subject2 + ":");
+    //    var obtainMarksSubject3 = prompt("Enter obtained marks for " + subject3 + ":");
+        
+    //     var totalObtainedMarks = obtainMarksSubject1 + obtainMarksSubject2 + obtainMarksSubject3;
+    //      var percentage = (totalObtainedMarks / (3 * totalMarks)) * 100;
+    //   document.write("<b>Result</b> <hr>");
+    //         document.write("<table>");
+    //         document.write("<tr><th>Subject</th><th>Total Marks</th><th>Obtained Marks</th></tr>");
+    //         document.write("<tr><td>" + subject1 + "</td><td>" + totalMarks + "</td><td>" + obtainMarksSubject1 + "</td></tr>");
+    //         document.write("<tr><td>" + subject2 + "</td><td>" + totalMarks + "</td><td>" + obtainMarksSubject2 + "</td></tr>");
+    //         document.write("<tr><td>" + subject3 + "</td><td>" + totalMarks + "</td><td>" + obtainMarksSubject3 + "</td></tr>");
+    //         document.write("</table>");
+    //         document.write("<p>Total Obtained Marks: " + totalObtainedMarks + "</p>");
+    //         document.write("<p>Percentage: " + percentage + "%</p>");
