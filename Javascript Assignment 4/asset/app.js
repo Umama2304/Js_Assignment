e8d38ca737f1f9306f2355abe761b35b7f7cc218
Umama2304 <129634648+Umama2304@users.noreply.github.com>
2023-05-17 14:15:16 +0500

    Add files via upload

---
+++ Javascript Assignment 4/asset/app.js
@@ -0,0 +1,19 @@
+// Chap 4 legal & illegal ;
+
+ // Task: 1;
+    // var firstName= "Umama Noor" ; city= "Karachi"; age= 20;
+
+ // Task: 2;
+    //  Legal names;
+    //  var name1 = "Umama Noor" ,  $city= "KARACHI", fav_snack= "Pizza ", _number= 20 ;
+
+    //  illegal names;
+    //   var, 1number, &now ,  @where,  const ;
+
+
+ // Task: 3;
+//     document.write ("<h1> Rules for naming JS variables </h1>" );
+// document.write("\n"+"Variable names can only contain,alphabets, numbers, dollar '$' signs and '_' . For example: $my_1stVariable" + "\n");
+// document.write ("<br> Variable must begin with a dollar '$' sign,  '_' or alphabet. For example $name, _name or name ");   
+//     document.write ( "<br>" +"Variable names are case sensitive" + "\n");       
+//     document.write ("Variable names should not be JS keyword.");
