69d0def6c8ad1a723869a7a770549f3c233be42c
Umama2304 <129634648+Umama2304@users.noreply.github.com>
2023-05-17 15:53:11 +0500

    Add files via upload

---
+++ Javascript Assignment 8/assets/app.js
@@ -0,0 +1,97 @@
+//Chap 12 and 13 IF…ELSE & ELSE IF STATEMENT, TESTING SET OF CONDITION;
+
+
+ //Task: 1;          
+//     var character = prompt("Enter a character:"); code = character.charCodeAt(0);
+//              if (code >= 48 && code <= 57) {
+//                 alert("The given input is a number.");
+//                 }
+//                  else if (code >= 65 && code <= 90) {
+//                 alert("The given input is an uppercase letter.");
+//                 }
+//                  else if (code >= 97 && code <= 122) {
+//                 alert("The given input is a lowercase letter.");
+//                 }
+    
+ //Task: 2; 
+            // var num1 = prompt("Enter the first integer:");
+            // var num2 = prompt("Enter the second integer:");
+
+            //     if (num1 > num2) {
+            //     alert ("The larger number is: " + num1);
+            //     } 
+            //     else if (num2 > num1) {
+            //     alert ("The larger number is: " + num2);
+            //     } else {
+            //     alert ("Both integers are equal.");
+            //     }
+
+    //Task: 3;
+        //     var number = prompt("Enter a  number to check positive or negative,and zero:");
+        //      if (number > 0) {
+        //           alert ("The number is positive.");
+        //         }
+        //              else if (number < 0) {
+        //             alert ("The number is negative.");
+        //             }
+        //              else {
+        //             alert ("The number is zero.");
+        //             }
+
+    //Task: 4; 
+        //       var character = prompt("Enter a character i.e. string of length 1:");
+        //         var isVowel = false;
+
+        //             if (
+        //             character === "a" || character === "e" ||
+        //             character === "i" || character === "o" ||
+        //             character === "u"
+        //             ) {
+        //             isVowel = true;
+        //             }
+
+        //              if (isVowel) {
+        //             alert ("The character is a vowel.");
+        //             }   
+        //                 else {
+        //             alert ("The character is not a vowel.");
+        //             }
+
+    //Task: 5;
+        //      const correctPassword = "password@12";
+        //        userPassword = prompt("Enter your password:");
+
+        //              if (userPassword === correctPassword) {
+        //             alert ("Correct! The password you entered matches the original password.");
+        //             } 
+        //             else {
+        //             alert ("Incorrect password.");
+        //             }
+
+             
+
+    //Task: 6; 
+        //    var greeting;  hour = 13;
+        //             if (hour < 18) {
+        //                 alert(greeting = "Good day" );
+        //             }
+        //             else { 
+        //                 alert ( greeting = "Good evening" );
+        //             }
+
+
+    //Task: 7; 
+        // var userTime = prompt ( "Enter your time here in 24-hour clock format like: 1900 = 7pm ");
+        //     if (userTime >= 0000 && userTime < 1200){
+        //         alert("Good morning!");
+        //     }
+        //     else if( userTime >= 1200 && userTime < 1700){
+        //         alert("Good afternoon!");
+        //     }
+        //     else if( userTime >= 1700 && userTime < 2100){
+        //         alert("Good evening!");
+        //     }
+        //     else if( userTime >= 2100 && userTime <= 2359){
+        //         alert("Good night!");
+        //     }
+    
