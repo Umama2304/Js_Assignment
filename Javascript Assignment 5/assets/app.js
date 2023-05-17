0080a690f9cc5dbbe204f3e9ec5634d1e3d86d7e
Umama2304 <129634648+Umama2304@users.noreply.github.com>
2023-05-17 14:15:54 +0500

    Add files via upload

---
+++ Javascript Assignment 5/assets/app.js
@@ -0,0 +1,100 @@
+// Chap 5 Math Expression;
+
+//Task: 1;
+// var num =  25; num_1 = 5; resultNumber = num + num_1;
+// document.write("Sum of " + num + " and " + num_1 + " is " + resultNumber );
+
+//Task : 2;
+// var number = 50;
+// subtraction= 10 - number;
+// document.write( "Subtraction is: " + subtraction);
+// var multiplication = 5 * number;
+// document.write("<br>Multiplication is:" + multiplication );
+// var division = 100 / 25;
+// document.write( "<br>Division is:" + division);
+// var modulus= 5 % 10;
+// document.write("<br>Modulus is:" + modulus);
+
+
+//Task : 3;
+// var city;
+// document.write ("Value after variable declaration is " + city);
+//  let number= 10;
+//  document.write( " <br>Initial value: "+ number);
+//  var increment = ++number;
+// document.write( " <br>Value after increment is : " + increment) ;
+//   var addNum = 7 + increment;
+// document.write("<br>Value after addition is  " + addNum );
+//    var decrement = --number;
+//   document.write("<br>Value after decrement is " + decrement ); 
+//   var remainder= number / 3;
+//   document.write("<br>The remainder is " + remainder );
+
+///Task : 4;
+// var costOfOne = 600 ;costOfFive = costOfOne * 5 ;
+// document.write("Total cost to buy 5 Tickets to a movie is " + costOfFive + "PKR <br>") ;
+
+//Task: 5  ;
+// var table = 2;
+//      for  ( var i = 1; i < 11; i++){
+//       document.write( table  + "x " + i + "=" + table * i + "<br>" );
+//       }  
+
+//Task : 6 ;
+//       var temperatureCelcius = 25; temperatureFahrenheit= 70;     
+//   document.write("<br>" + temperatureCelcius + "C " + "is " + ((temperatureCelcius * 9/5) + 32) + " F" );
+//  document.write( "<br>" + temperatureFahrenheit + "F "+ "is " + ((temperatureFahrenheit -32) * 5/9) + " C");
+
+
+//Task : 7 ;
+//   var priceItem1 = 650; priceItem2 = 100; quantityItem1 = 3; quantityItem2 = 7; charges = 100;
+//   var totalItem1 = priceItem1 * quantityItem1; 
+//   var totalItem2 = priceItem2 * quantityItem2;
+//   var totalCost= totalItem1 + totalItem2 + charges;
+//   document.write("<h4> Shopping Cart </h4>");
+//   document.write("<br> Price of item 1 is " + priceItem1 );
+//   document.write("<br>Quantity of item 1 is " + quantityItem1);
+//   document.write( "<br> Price of item 2 is" + priceItem2 );
+//   document.write("<br>Quantity of item 2 is " + quantityItem2 );
+//   document.write("<br> Shipping Charges" + charges );
+//   document.write( "<br> <br> Total cost of your order is " + totalCost  );
+  
+//Task : 8 ;
+//  var totalMarks = 980; obtainMarks = 805; percentage = obtainMarks / totalMarks * 100;
+//   document.write("<h1> MarkSheet </h1> " );
+//  document.write ( "Total marks: " + totalMarks + "<br> Marks Obtained: " + obtainMarks + "<br> Percentage: " + percentage); 
+
+
+//Task: 9;
+//    var dollar = 104.80 ; riyal = 28 ; 
+//    totalCurrency = (dollar * 10 ) + ( riyal * 25 );
+//    document.write("<h1> Currency in PKR </h1>"); 
+//    document.write( " Toal Currency in PKR: " + totalCurrency );
+
+//Task :10 ;
+//    var num = 6 ; addition = num + 5; multiply= num * 10; divide = num / 2  ;
+//    console.log(num);
+   
+// Task: 11;
+//    var currentYear = 2023; birthYear = 2002;  calulate =  currentYear -  birthYear;
+//    document.write("<h1> Age Calculator </h1>");
+//    document.write ( " Current year: " + currentYear + "<br> Birth Year "+ birthYear +
+//    "<br>Your Age is: " +  calulate);
+
+
+//Task: 12;
+    // var radius = 20;
+    // document.write("<h3>The Geometrizer</h3>");
+    // document.write("Radius of a circle : " + radius + "<br>");
+    // document.write("The circumference is : " + (2 * 3.142 * radius) + "<br>");
+    // document.write("The area is : " + 3.142 * (radius * radius) );
+
+// Task:13; 
+    // var snack = " Strawberry Ice-cream "; currentAge = 20; maximumAge = 60;  perDay = 2 ;
+    // var totalSnacks = (maximumAge - currentAge) * 365 * perDay;
+    // document.write("<h1> LifeTime Supply Calulator </h1>");
+    // document.write("Favourite Snack: " + snack );
+    // document.write(  "<br>Current Age: " + currentAge ); 
+    // document.write("<br>Maximum Age: " + maximumAge + "<br> Estimated amount per day " + perDay);
+
+    // document.write("<br> You will need " + totalSnacks + snack + " to last you until the ripe old age of " + maximumAge );
