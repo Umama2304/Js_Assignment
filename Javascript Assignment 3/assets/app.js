0ab00374fad068ccf8b599204b9bc1bbf41ebf57
Umama2304 <129634648+Umama2304@users.noreply.github.com>
2023-05-17 14:14:39 +0500

    Add files via upload

---
+++ Javascript Assignment 3/assets/app.js
@@ -0,0 +1,20 @@
+//chap 3 Variable Numbers;
+
+//Task : 1
+//var age = 24 ;
+//alert( "I am " + age  + " Years Old");
+
+// Task :2
+// var visitcount = 15;
+// alert( "You have visited this site " + visitcount  + " times") ;
+
+// Task :3
+// let birthYear = 2002 ;
+// document.write(" My Birth Year is " +  birthYear );
+// document.write( "Data type of my declared variable is " + typeof (birthYear));
+
+// Task: 4 
+//  let visitorName = "John Doe";
+//  let productTitle = "T-shirt(s)";
+//  let quantity= 5;
+//  document.write ( "<strong>" + visitorName +  "</strong>" + " ordered " + "<strong>"+ quantity + "</strong>" + " " + productTitle + " on XYZ Clothing store .")
